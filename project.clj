73a5609c1c75ac9eb8ddc785f629991f3584b593
ackerleytng <ackerleytng@gmail.com>
2019-03-27 07:05:11 +0800

    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject segment "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.0-RC2"]
-                 [org.clojure/data.zip "0.1.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.zip "0.1.3"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/tools.cli "0.4.1"]
                  [hickory "0.7.1"]]
